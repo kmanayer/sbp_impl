4b4b4c3455e896740c6f1daf07981682a1e6f221
Kirellos N. Abou Elsaad <kmanayer@yahoo.com>
2022-06-26 14:47:03 -0400

    second commit - masterSecret field is null

--- HTTPS_Server.java
+++ HTTPS_Server.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyStore;
 
 public class HTTPS_Server {
@@ -20,10 +21,12 @@ public class HTTPS_Server {
             String Response = "This is the response from delftstack";
             HttpsExchange xs = (HttpsExchange) x;
             //DatatypeConverter.printHexBinary(xs.getSSLSession().getId()); // byte array has 32 bytes
-            printByteArray(xs.getSSLSession().getId());
+            System.out.println(bytesToHex(xs.getSSLSession().getId()));
+            //printByteArray(xs.getSSLSession().getId());
             try {
                 Class<?> c = Class.forName("sun.security.ssl.SSLSessionImpl");
                 Field masterSecretField = c.getDeclaredField("masterSecret");
+                masterSecretField.setAccessible(true);
                 SecretKey k = (SecretKey)masterSecretField.get(xs.getSSLSession());
                 printByteArray(k.getEncoded());
             } catch (Exception e) {
@@ -42,6 +45,18 @@ public class HTTPS_Server {
             System.out.print((int)a[i] + " ");
         }
         System.out.println();
+    }
+
+    //https://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
+    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+    public static String bytesToHex(byte[] bytes) {
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars, StandardCharsets.UTF_8);
     }
 
     public static void main(String[] args) throws Exception {
